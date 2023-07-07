c43d7ce26ef6562894b83d72a20f4ce084bfe5a5
Jacob Lai <laijs1@uci.edu>
2023-07-07 16:31:03 -0700

    bs4 attempt - fail

---
+++ scraper/scraper.py
@@ -0,0 +1,20 @@
+# Result was unattainable. -> Swapping to Selenium
+# import requests
+# from bs4 import BeautifulSoup
+
+# # Making a GET request
+# r = requests.get('https://www.ycombinator.com/companies/')
+
+# # Parsing the HTML
+# soup = BeautifulSoup(r.content, 'html.parser')
+
+# print(soup.title)
+
+# z = soup.find('span', class_="CBY8yVfV0he1Zbv9Zwjx")
+# print(z)
+# companyNames = soup.find_all('span', class_='CBY8yVfV0he1Zbv9Zwjx')
+
+# for companyName in companyNames:
+#     print(companyName.text)
+# print('Complete')
+
