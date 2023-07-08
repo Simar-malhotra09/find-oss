4596040f36317c29915229df14c77c5cd80d9a6f
Jacob Lai <laijs1@uci.edu>
2023-07-07 18:41:48 -0700

    display company names

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -20,6 +20,9 @@
 
 # Selenium Attempt
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import json
 
 driver = webdriver.Chrome()
@@ -28,6 +31,19 @@ driver.get("https://www.ycombinator.com/companies/")
 # The YC Startup Directory | Y Combinator
 print(driver.title)
 
+# Force scroll (to see all 1000 companies) -- NOT WORKING
+iterations = 10
+for i in range(iterations):
+    driver.find_element("tag name", "body").send_keys(Keys.END)
+    driver.implicitly_wait(2)
+
+elements = driver.find_elements(By.CLASS_NAME, "CBY8yVfV0he1Zbv9Zwjx")
+
+print(f'Number of elements found: {len(elements)}')
+print(elements)
+
+for e in elements:
+    print(e.text)
 
 
 driver.quit()
