07a6d40904eff75d8214495116b04bf3450cb541
Jacob Lai <laijs1@uci.edu>
2023-07-07 20:03:40 -0700

    clean up scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,40 +1,13 @@
-# Result was unattainable. -> Swapping to Selenium
-# import requests
-# from bs4 import BeautifulSoup
-
-# # Making a GET request
-# r = requests.get('https://www.ycombinator.com/companies/')
-
-# # Parsing the HTML
-# soup = BeautifulSoup(r.content, 'html.parser')
-
-# print(soup.title)
-
-# z = soup.find('span', class_="CBY8yVfV0he1Zbv9Zwjx")
-# print(z)
-# companyNames = soup.find_all('span', class_='CBY8yVfV0he1Zbv9Zwjx')
-
-# for companyName in companyNames:
-#     print(companyName.text)
-# print('Complete')
-
-# Selenium Attempt
 import time
 import json
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
-
-
 def webScrape() -> list:
     driver = webdriver.Chrome()
     driver.get("https://www.ycombinator.com/companies/")
-
-    # The YC Startup Directory | Y Combinator
-    # print(driver.title)
 
     # Force scroll (to see all 1000 companies)
     iterations = 100
@@ -45,15 +18,9 @@ def webScrape() -> list:
     # Find all company names
     elements = driver.find_elements(By.CLASS_NAME, "CBY8yVfV0he1Zbv9Zwjx")
 
-
-    # print(f'Number of elements found: {len(elements)}') # -- confirmed: 1000 elements extracted
-    # print(elements)
-
     # Aggregate all company names into list
     names = [e.text for e in elements]
-    
     driver.quit()
-    
     return names
 
 def convertJSON(names: list) -> None:
