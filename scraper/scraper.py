aa63994ed1d1886ac68db5faa6254cbe84f1fb05
Jacob Lai <laijs1@uci.edu>
2023-07-07 19:08:25 -0700

    complete webScrape function

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -25,25 +25,32 @@ from selenium.webdriver.common.by import By
 import time
 import json
 
-driver = webdriver.Chrome()
-driver.get("https://www.ycombinator.com/companies/")
+def webScrape() -> list:
+    driver = webdriver.Chrome()
+    driver.get("https://www.ycombinator.com/companies/")
 
-# The YC Startup Directory | Y Combinator
-print(driver.title)
+    # The YC Startup Directory | Y Combinator
+    # print(driver.title)
 
-# Force scroll (to see all 1000 companies) -- NOT WORKING
-iterations = 10
-for i in range(iterations):
-    driver.find_element("tag name", "body").send_keys(Keys.END)
-    driver.implicitly_wait(2)
+    # Force scroll (to see all 1000 companies)
+    iterations = 100
+    for _ in range(iterations):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)") # scroll to the end of the page
+        time.sleep(0.18) # wait time to allow site to load
 
-elements = driver.find_elements(By.CLASS_NAME, "CBY8yVfV0he1Zbv9Zwjx")
+    # Find all company names
+    elements = driver.find_elements(By.CLASS_NAME, "CBY8yVfV0he1Zbv9Zwjx")
 
-print(f'Number of elements found: {len(elements)}')
-print(elements)
+    driver.quit()
 
-for e in elements:
-    print(e.text)
+    # print(f'Number of elements found: {len(elements)}') # -- confirmed: 1000 elements extracted
+    # print(elements)
+
+    # Aggregate all company names into list
+    names = [e.text for e in elements]
+
+    return names
 
 
-driver.quit()
+if __name__ == '__main__':
+    companyNames = webScrape()
